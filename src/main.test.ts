1d44fe309ac7219b0be4eb3c5d30244824d39458
SerKo <serko.dev@gmail.com>
2022-04-16 19:37:04 +0800

    test: update test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,8 +1,9 @@
 import bcd from "@mdn/browser-compat-data";
 import mdnCompat from "./main";
 
-const compat = new mdnCompat();
+const compat = new mdnCompat(["> 0.5%", "last 2 versions", "not dead"]);
 
 // TODO: test case
 console.log(compat.unsupport(bcd.css.properties));
-console.log(compat.unsupport(bcd.css.selectors));
+console.log(compat.unsupport(bcd.javascript.operators));
+console.log(compat.unsupport(bcd.css.properties["justify-content"]));
